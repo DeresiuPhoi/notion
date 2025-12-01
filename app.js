75f3f796fec84c6c4c6288a42bf3b41a4bb6e6d5
Nurlann Musepov <mussepovn@gmail.com>
2025-12-02 04:52:35 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,72 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+
+const app = express()
+const port = 3000
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "note_pro",
+  password: "1234",
+  port: 5433,
+});
+db.connect();
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await db.query("SELECT * FROM notion"); // columns: id, isbn, title, note
+    res.render("main_page.ejs", { notions: result.rows });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Database error");
+  }
+});
+
+app.get("/add", (req, res) => {
+  res.render("add.ejs");
+});
+
+app.post("/add", async(req,res)=>{
+  const isbn = req.body["isbn"];
+  const title = req.body["title"];
+  const note = req.body["note"];
+  try{
+    await db.query("INSERT INTO notion (title, note,isbn) VALUES ($1, $2, $3)", [
+      title, note, isbn])
+    console.log("new notion added")
+    res.redirect("/")
+  }catch(err){
+    console.log(err)
+    res.redirect("/")
+  }
+})
+
+app.get("/update", (req, res) => {
+  res.render("add_note.ejs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("add_note.ejs");
+});
+
+
+// app.post("/submit", (req, res) => {
+//   const randomAdj = adj[Math.floor(Math.random() * adj.length)];
+//   const randomNoun = noun[Math.floor(Math.random() * noun.length)];
+//   res.render("solution.ejs", {
+//     adjective: randomAdj,
+//     noun: randomNoun,
+//   });
+// });
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
