8e1ce649375f0ad5abdc311947db7122dc966030
Nurlann Musepov <mussepovn@gmail.com>
2025-12-19 00:10:16 +0500

    Add dotenv

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
-
+import env from "dotenv";
 
 const app = express()
 const port = 3000
@@ -11,11 +11,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "note_pro",
-  password: "1234",
-  port: 5433,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 });
 db.connect();
 
